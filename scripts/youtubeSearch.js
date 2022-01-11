3f3d0f7f6209113e22610ddeca32decf2c85b978
Jackie <taekwondo.chan@gmail.com>
2022-01-10 20:34:47 -0800

    resolve with proper urls

--- scripts/youtubeSearch.js
+++ scripts/youtubeSearch.js
@@ -1,6 +1,6 @@
 const youtubeSearchApi = require("youtube-search-api");
 
-const testFunc = (keywordsArray) => {
+let searchYoutube = (keywordsArray) => {
 	return new Promise((resolve, reject) => {
 		let urls = [];
 		let promises = [];
@@ -8,21 +8,23 @@ const testFunc = (keywordsArray) => {
 		for (let i in keywordsArray) {
 			promises.push(
 				new Promise((resolve, reject) => {
-					youtubeSearchApi.GetListByKeyword(keywordsArray[i], false, 1).then((res) => {
-						console.log(res["items"][0]["id"]);
-						resolve(urls.push(res["items"][0]["id"]));
-					});
+					youtubeSearchApi
+						.GetListByKeyword(keywordsArray[i], false, 1)
+						.then((res) => {
+							resolve(urls.push(`www.youtube.com/watch?v=${res["items"][0]["id"]}`));
+						})
+						.catch((err) => reject(err));
 				})
 			);
 		}
 
-		Promise.all(promises).then(() => {
+		Promise.allSettled(promises).then(() => {
 			resolve(urls);
 		});
 	});
 };
 
-testFunc([
+searchYoutube([
 	"BEcause - Dreamcatcher",
 	"Bossfight - Flirt Flirt Oh It Hurts",
 	"Jay Wolf - Telepathy",

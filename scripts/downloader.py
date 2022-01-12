e2b2b72a0d685ef525b7b6308059d863c2897334
Jackie <taekwondo.chan@gmail.com>
2022-01-11 16:36:57 -0800

    add pytube script

---
+++ scripts/downloader.py
@@ -0,0 +1,14 @@
+import sys
+import pytube
+
+videoUrl = str(sys.argv[1])
+videoId = videoUrl.split("?v=")[1]
+
+try:
+    pytube.YouTube(videoUrl).streams.filter(only_audio=True).first().download(
+        filename=f"{videoId}.mp4", output_path="./audio")
+    print(f'{videoUrl} download complete')
+except:
+    print(f'{videoUrl} download failed')
+
+sys.stdout.flush()

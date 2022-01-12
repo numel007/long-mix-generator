f0eeb5fd8a467a6a1f91974a5c4faf1b38ef5621
Jackie <taekwondo.chan@gmail.com>
2022-01-11 19:34:20 -0800

    concat audio tracks and export

--- scripts/spliceAudio.js
+++ scripts/spliceAudio.js
@@ -9,17 +9,24 @@ const spliceAudio = () => {
 				reject({ err: "Unable to get list of downloaded videos" });
 			} else {
 				promises = [];
+				filenames = [];
 				files.forEach((file) => {
 					promises.push(
 						new Promise((resolve, reject) => {
+							// Convert video file to mp3
 							ffmpeg(`./audio/${file}`)
 								.audioFilters("silenceremove=stop_periods=2")
 								.format("mp3")
 								.save(`./audio/${file.slice(0, -4)}.mp3`)
-								.on("err", () => console.log(`Error converting ${file}`))
+								.on("err", (err) => console.log(`Error converting ${file}`))
 								.on("start", () => console.log(`Converting ${file}`))
 								.on("end", () => {
-									console.log(`${file} complete`);
+									// Delete unconverted video file
+									fs.unlink(`${__dirname}/audio/${file}`, (err) => {
+										if (err) throw err;
+									});
+
+									filenames.push(`${file.slice(0, -4)}.mp3`);
 									resolve();
 								});
 						})
@@ -27,12 +34,38 @@ const spliceAudio = () => {
 				});
 
 				Promise.allSettled(promises).then(() => {
-					console.log("Conversions completed");
 					// Begin splicing audio together
+
+					// Get list of mp3s and add to input
+					let mergedAudio = ffmpeg();
+					filenames.forEach((filename) => {
+						mergedAudio.addInput(`${__dirname}/audio/${filename}`);
+					});
+
+					// Merge input files, output to ./output/mergedAudio.mp3
+					mergedAudio
+						.mergeToFile(`${__dirname}/output/mergedAudio.mp3`, "./tmp/")
+						.on("err", (err) => console.log(err))
+						.on("start", () => console.log("Merging audio tracks."))
+						.on("end", () => {
+							console.log("Finished merging audio.");
+
+							// Delete mp3s
+							fs.readdir(`${__dirname}/audio`, (err, files) => {
+								files.forEach((file) => {
+									fs.unlink(`${__dirname}/audio/${file}`, (err) => {
+										if (err) throw err;
+									});
+								});
+							});
+
+							// All operations have completed
+							resolve();
+						});
 				});
 			}
 		});
 	});
 };
 
-spliceAudio();
+spliceAudio().then((res) => console.log(res));

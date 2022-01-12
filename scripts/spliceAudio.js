bb799176ae8ba274d6cd1df78928de1dbf68947d
Jackie <taekwondo.chan@gmail.com>
2022-01-11 18:34:18 -0800

    convert into mp3 files

--- scripts/spliceAudio.js
+++ scripts/spliceAudio.js
@@ -8,14 +8,27 @@ const spliceAudio = () => {
 			if (err) {
 				reject({ err: "Unable to get list of downloaded videos" });
 			} else {
+				promises = [];
 				files.forEach((file) => {
-					console.log(file);
+					promises.push(
+						new Promise((resolve, reject) => {
+							ffmpeg(`./audio/${file}`)
+								.format("mp3")
+								.save(`./audio/${file.slice(0, -4)}.mp3`)
+								.on("err", () => console.log(`Error converting ${file}`))
+								.on("start", () => console.log(`Converting ${file}`))
+								.on("end", () => {
+									console.log(`${file} complete`);
+									resolve();
+								});
+						})
+					);
 				});
+
+				// Conversions completed
+				Promise.allSettled(promises).then(() => resolve(console.log("All conversions finished.")));
 			}
 		});
-
-		// Conversion and splice is complete
-		resolve();
 	});
 };
 

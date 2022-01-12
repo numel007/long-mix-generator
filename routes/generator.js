f4da42e7a5690e2b08f5e982f4c6b898828fca98
Jackie <taekwondo.chan@gmail.com>
2022-01-12 00:20:11 -0800

    delete old audio files before script runs

--- routes/generator.js
+++ routes/generator.js
@@ -5,6 +5,7 @@ const { getSongNames } = require("../scripts/spotifySearch");
 const { searchYoutube } = require("../scripts/youtubeSearch");
 const { youtubeDownload } = require("../scripts/youtubeDownload");
 const { spliceAudio } = require("../scripts/spliceAudio");
+const fs = require("fs");
 
 router.get("/generate", (req, res) => {
 	return res.json({ message: "Hello from /generate" });
@@ -12,6 +13,15 @@ router.get("/generate", (req, res) => {
 
 // TODO: Should pass genre in req body
 router.post("/generate", (req, res) => {
+	// Delete mergedAudio.mp3 before running new conversion
+	fs.unlink(`${__dirname}/../scripts/output/mergedAudio.mp3`, (err) => {
+		if (err && err.code == "ENOENT") {
+			console.log("mergedAudio.mp3 doesn't exist. Nothing to remove.");
+		} else {
+			console.log("mergedAudio.mp3 deleted");
+		}
+	});
+
 	getAccessToken()
 		.then((token) => {
 			// TODO: Remove hardcoded ambient genre

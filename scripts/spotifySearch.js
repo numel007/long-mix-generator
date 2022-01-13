23c34aa98a18dce0425d7b3f3c3a73aa12db32fe
Jackie <taekwondo.chan@gmail.com>
2022-01-13 13:22:54 -0800

    detect unsupported genre

--- scripts/spotifySearch.js
+++ scripts/spotifySearch.js
@@ -1,6 +1,7 @@
 const request = require("request");
 
 exports.getSongNames = (accessToken, genre) => {
+	console.log(accessToken);
 	var options = {
 		method: "GET",
 		url: `https://api.spotify.com/v1/recommendations?limit=5&seed_genres=${genre}`,
@@ -11,16 +12,17 @@ exports.getSongNames = (accessToken, genre) => {
 
 	return new Promise((resolve, reject) => {
 		request(options, function (error, response) {
-			if (error) {
-				reject({ err: "Failed to retrieve songs" });
-			}
-
 			let names = [];
 			let tracks = JSON.parse(response.body)["tracks"];
+
+			if (tracks.length == 0) {
+				reject({ err: "Bad genre" });
+			}
+
 			for (let track in tracks) {
 				names.push(`${tracks[track]["name"]} - ${tracks[track]["artists"][0]["name"]}`);
 			}
-			resolve(names);
+			resolve({ names: names });
 		});
 	});
 };
